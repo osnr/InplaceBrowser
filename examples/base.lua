baaa2fdef1523dfa286952c3a68711a09be0f6c7
Omar Rizwan <omar@omar.website>
2024-09-13 12:26:16 -0400

    Squashed 'vendor/luajit-glfw/' content from commit 1804699
    git-subtree-dir: vendor/luajit-glfw
    git-subtree-split: 1804699ac3c21d655c62f191d013c77830c413da

---
+++ examples/base.lua
@@ -0,0 +1,30 @@
+local glfw = require 'glfw' ('glfw3')
+local GLFW = glfw.const
+
+-- Initialize the library
+if glfw.Init() == 0 then
+  return
+end
+
+-- Create a windowed mode window and its OpenGL context
+local window = glfw.CreateWindow(640, 480, "Hello World")
+if window == GLFW.NULL then
+  glfw.Terminate()
+  return
+end
+
+-- Make the window's context current
+glfw.MakeContextCurrent(window)
+
+-- Loop until the user closes the window
+while glfw.WindowShouldClose(window) == 0 do
+  -- Render here
+
+  -- Swap front and back buffers
+  glfw.SwapBuffers(window)
+
+  -- Poll for and process events
+  glfw.PollEvents()
+end
+
+glfw.Terminate()
